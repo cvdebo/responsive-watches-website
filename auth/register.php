8a7bb40030784fcf52129910ad0b633847027340
Abd elfatah Abd elhammed <129082371+cvdebo@users.noreply.github.com>
2023-03-28 00:01:11 +0000

    update

---
+++ auth/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive watches website</title>
+</head>
+<body>
+
+    <section class="form">
+        <form action="#" method="post">
+            <p>your name<span>*</span></p>
+            <input type="text" placeholder="enter your name" name="name" required>
+            <p>your email<span>*</span></p>
+            <input type="email" placeholder="enter your email" name="email" required>
+            <p>your password<span>*</span></p>
+            <input type="password" placeholder="enter your password" name="password" required>
+            <p>your confirm password<span>*</span></p>
+            <input type="password" placeholder="enter your confirm  password" name="password" required>
+            <input type="submit" value="Register" name="register">
+            <p>hava an acount? <a href="login.php">Login now</a></p>
+        </form>
+    </section>
+    
+</body>
+</html>
